6215ba7095139b10566b3fd602af1e84eec51ae5
Reeman <R12eeman.a78@gmail.com>
2025-10-28 01:17:54 +0300

    Initial Commit

---
+++ Plant/PlantApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantApp.swift
+//  Plant
+//
+//  Created by Reeman Aqeel on 06/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct PlantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantTests/PlantTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantTests.swift
+//  PlantTests
+//
+//  Created by Reeman Aqeel on 06/05/1447 AH.
+//
+
+import Testing
+@testable import Plant
+
+struct PlantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
